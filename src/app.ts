69caf0797c4572af7d9346a77ec96c9a15a27c54
pradeeptata04 <2100030875cseh@gmail.com>
2026-02-04 21:24:52 +0530

    7th commit

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: ['http://localhost:5173', 'https://webileapps.netlify.app'], // Local and Production
     credentials: true
 }));
 app.use(helmet());
