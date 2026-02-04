bd3aa546e8c39f54b0c62cadba500453d384d8b0
pradeeptata04 <2100030875cseh@gmail.com>
2026-02-04 21:55:39 +0530

    10th commit

--- src/app.ts
+++ src/app.ts
@@ -13,12 +13,16 @@ import { globalDecryption, globalEncryption } from './middleware/encryptionMiddl
 const app = express();
 
 // Middleware
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: '*', // Allow ALL origins
-    credentials: false // Disable credentials to allow wildcard
-}));
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cookieParser());
